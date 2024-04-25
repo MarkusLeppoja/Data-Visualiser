663073f39a76ff852f3e95a74a62aa6b69c9f7f1
YTFurys <markusleppoja4@gmail.com>
2024-04-25 20:18:59 +0300

    Add project files.

---
+++ Pages/Motor/MotorDataView.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Data_Visualiser.Pages.Motor
+{
+    public class MotorDataViewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
